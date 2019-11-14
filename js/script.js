cddea1bd3b2be5ee780d9bfa52a9ca29029b57ab
bolohov <bolohov.vlad.95@gmail.com>
2019-11-14 13:00:03 +0200

    add

---
+++ js/script.js
@@ -0,0 +1,143 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+    //tabs
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+    
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++){
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+    hideTabContent(1);
+
+    
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')){
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(e){
+        let target = e.target;
+
+        if (target && target.classList.contains('info-header-tab')){
+            for (let i = 0; i < tab.length; i++) {
+                if(target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+    //timer
+
+    //1) deadline
+    let deadline = '2019-11-12';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()); //кол милисек.
+
+        let seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            // hours = Math.floor((t/1000/60/60) % 24),
+            // days = Math.floor((t/(1000*60*60*24))),
+            hours = Math.floor((t/(1000*60*60)));
+
+            return {
+                'total' : t,
+                'hours' : hours,
+                'minutes' : minutes,
+                'seconds' : seconds
+            };
+    };
+
+    function setClock (id, endtime ) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+        let timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            if (t.seconds < 10) {
+                seconds.textContent = '0' + t.seconds;
+            } else if (t.minutes < 10) {
+                minutes.textContent = '0' + t.minutes;
+            } else if (t.hours < 10) {
+                hours.textContent = '0' + t.hours;
+            } else {
+                hours.textContent = t.hours;
+                minutes.textContent = t.minutes;
+                seconds.textContent = t.seconds;
+            }
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+    setClock('timer', deadline);
+
+    //Modal window
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close'),
+        about = document.querySelector('#about');
+        
+   
+    about.addEventListener('click', function(e) {
+        if (e.target && e.target.className == 'more' || e.target.className == 'description-btn'){
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        }
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+    
+
+    class Options {
+        constructor(height, width, bg, fontSize, textAlign) {
+          this.height = height;
+          this.width = width;
+          this.bg = bg;
+          this.fontSize = fontSize;
+          this.textAlign = textAlign;
+        }
+
+        newDiv() {
+            var el = document.createElement("div");
+
+            el.textContent = 'Привет';
+            el.style.cssText = `
+            height: ${this.height};
+            width: ${this.width};
+            background-color: ${this.bg};
+            font-size: ${this.fontSize};
+            text-align: ${this.textAlign};
+          `;
+
+          console.log(el);
+        }
+    }
+    const square = new Options('10px', '100px', 'red', '14px', 'center');
+    square.newDiv();
+
+});
